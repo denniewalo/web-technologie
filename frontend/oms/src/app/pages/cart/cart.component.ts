f4378d7e1fefce0c73ad325eb20c3c637c8688f1
sarina <sarina.regenauer@web.de>
2022-01-21 19:16:08 +0100

    update buy-method of cart

--- frontend/oms/src/app/pages/cart/cart.component.ts
+++ frontend/oms/src/app/pages/cart/cart.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {CartService} from "../../services/cart.service";
 import {Product} from "../../interfaces/Product";
+import { Router } from '@angular/router';
+//import { AuthService } from 'src/app/services/authService/auth.service';
 
 @Component({
   selector: 'app-cart',
@@ -10,7 +12,9 @@ import {Product} from "../../interfaces/Product";
 export class CartComponent implements OnInit {
   cart : Product[] = []
   constructor(private cartService: CartService,
-              //private orderService: OrderService
+              private router: Router,
+              //private orderService: OrderService,
+              //private authService: AuthService
               ) { }
 
   ngOnInit(): void {
@@ -26,11 +30,23 @@ export class CartComponent implements OnInit {
     return this.cartService.getCartPrice()
   }
 
-  /**
-   * buy(){
-   * //if not logged in -> route login
-   * //else
-   * this.orderService.createOrder()
-   * }
-   */
+  buy(){
+    this.router.navigate(['/login']);
+    }
+   
+
+/**
+ *
+     buy(){
+    //check is user is logged in
+    if(!this.authService.isLoggedIn()){
+     this.router.navigate(['/login']);
+   }
+
+   else{
+     this.orderService.createOrder(this.authService.getUserId(), this.cartService.getCart(), this.cartService.getCartPrice(),"In progess");
+      this.cartService.clearCart();
+    }
+  }
+ */
 }
